7498256ba7819933ee606befcc2e6a311fcacac6
Dave Beidle <dbeidle@gmail.com>
2021-10-26 05:19:07 -0500

    Added Vue logo and copyright owner for video

--- src/App.vue
+++ src/App.vue
@@ -3,7 +3,11 @@
     <h1 @click="onClick">HELLO WORLD!</h1>
     
     <VideoView />
-    
+        <p>This video is owned and copyrighted by <a href="https://www.capitolrecords.com/#/">Capitol Records</a></p>
+
+    <div>
+      <p>Powered by:</p>
+    <img class="image" src="vue_logo.png" alt="Vue JS Logo"/></div>
   </div>
 </template>
 
@@ -37,5 +41,20 @@
     justify-content:center;
     align-items:center; 
     padding: 20px;
-}
+    cursor: pointer;
+  }
+  p {
+     
+      text-align: center; 
+  }
+  .image {
+    display: block;
+    margin: auto;
+    height: 15%;
+    width: 15%;
+  }
+  a {
+    color: white;
+    text-decoration: none;
+  }
 </style>
--- src/components/VideoView.vue
+++ src/components/VideoView.vue
@@ -1,6 +1,7 @@
 <template>
     <div>
         <iframe width="840" height="472" src="https://www.youtube.com/embed/al2DFQEZl4M" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+   
     </div>
 </template>
 
